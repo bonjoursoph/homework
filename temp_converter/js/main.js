4d0a996f06e7bea263b6f40eb513fb98a9e0e90e
bonjoursoph <sophieshen@sophieshen-macbookair.roam.corp.google.com>
2016-04-05 21:39:56 -0700

    FINAL

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,27 @@
+// User enters in field
+// User hits F to C button
+
+$('#fahrenheit_to_celsius').on('click', function () {
+	var fahrenheit = $("#temperature").val();
+	fahrenheit = parseFloat(fahrenheit);
+	console.log(fahrenheit);
+
+	var celsius = (fahrenheit - 32)/1.8;
+	console.log(celsius);
+
+	$("#result").html(celsius);
+});
+
+
+
+
+
+
+// Store number as var
+// Convert var to number
+// Calculate to C
+// Change HTML to display new number
+
+
+
 
